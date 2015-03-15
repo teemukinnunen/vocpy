38aac38054b266e088f8e5e0950f77f2199be2f6
Teemu Kinnunen <jte.kinnunen@gmail.com>
2015-03-15 14:30:50 +0200

    Now it can extract features from images in a directory or from a single image

--- tools/extractfeatures.py
+++ tools/extractfeatures.py
@@ -35,8 +35,6 @@ def localfeatures_save(ips, lfs, imageFile, dataDir, detector, descriptor):
                             detector + '+' + descriptor,
                             imageFile)
 
-    print("datafile: " + dataFile)
-
     # Convert OpenCV keypoints to a matrix
     ips = localfeatures_kp2array(ips)
 
@@ -55,44 +53,38 @@ def main(argv):
 
     # Parse command line parameters
     parser = argparse.ArgumentParser(description='This script extracts visual features from given images')
-    parser.add_argument('-i', '--image', help="Path to the image", required=False)
-    parser.add_argument('-id', '--imageDir', default=None, help='Input image directory')
-    parser.add_argument('-dd', '--dataDir', default=None, help='Data directory where data will be stored')
+    parser.add_argument('-i', '--imageDir', help="Path to the image or directory", required=True)
+    parser.add_argument('-dd', '--dataDir', help='Data directory where data will be stored', required=True)
     parser.add_argument('-il', '--imageList', default=None, help='Input image list file')
     parser.add_argument('-ip', '--detector', default="HARRIS", help='Local feature detector (HARRIS, SIFT, ...)')
     parser.add_argument('-lf', '--descriptor', default="SIFT", help="Local feature descriptor")
 
     args = parser.parse_args()
 
-    # If user wants to extracts features from many images
-    if args.imageDir is not None:
-        # dataDir must be given with imageDir
-        if not args.dataDir:
-            parser.error("--dataDir must be also given with --imageDir")
-        # Get a list of images
-        if args.imageList is None:
+    # Get a list of images
+    if args.imageList is None:
+        # If user wants to extract features from a single image
+        if args.imageDir.endswith('.jpg') or args.imageDir.endswith('.png'):
+            imgList = [args.imageDir]
+        else:
             # Get a list of images from the given directory
             imgList = glob.glob(args.imageDir + '*/*.jpg')
-        else:
-            # Read the given imageList
-            imgList = open(args.imageList).read()
-        print("Image Dir given: " + args.imageDir)
-        # Start extracting features and saving them in the
-        for imgid in range(0, len(imgList)):
-            imageFile = imgList[imgid]
-            print("Processing %s (%d/%d)" % (imageFile, imgid, len(imgList)))
-            imageFilePath = os.path.join(imageFile[len(args.imageDir):])
-            [ips, lfs] = lf.extractfeatures(imageFile,
-                                        detector=args.detector,
-                                        descriptor=args.descriptor)
-            localfeatures_save(ips, lfs, imageFilePath, args.dataDir, args.detector, args.descriptor)
-
-    # If user wants to extract features from one specific image
-    elif args.image is not None:
-        print("Image given: " + args.image)
     else:
-        parser.error("Image OR image directory must be given!")
+        # Read the given imageList
+        imgList = open(args.imageList).read()
 
+    # This is the actual imageDir in every case
+    imgDir = os.path.dirname(args.imageDir)
+
+    # Start extracting features and saving them in the
+    for imgid in range(0, len(imgList)):
+        imageFile = imgList[imgid]
+        sys.stdout.write("Processing %s (%d/%d)\r" % (imageFile, imgid, len(imgList)))
+        imageFilePath = os.path.join(imageFile[len(imgDir)+1:])
+        [ips, lfs] = lf.extractfeatures(imageFile,
+                                    detector=args.detector,
+                                    descriptor=args.descriptor)
+        localfeatures_save(ips, lfs, imageFilePath, args.dataDir, args.detector, args.descriptor)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
