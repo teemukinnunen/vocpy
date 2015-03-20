8d74ebf1191b10bf84aad0a6ad3b7bd8dd5e735f
Teemu Kinnunen <teemu.kinnunen@ovi.com>
2015-03-20 20:40:11 +0200

    Filepaths fixed and now scripts are using ImageCollection class from datasets

--- tools/extractfeatures.py
+++ tools/extractfeatures.py
@@ -5,48 +5,10 @@
 import os
 import sys
 import argparse
-import numpy as np
-import glob
 
 # Append vocpy scripts
-sys.path.append('../')
-from localfeatures import LocalFeatures as lf
-
-def localfeatures_kp2array(ips):
-    """Converts keypoint vector to array matrix"""
-    A = np.zeros((len(ips), 7))
-
-    for i in range(0, len(ips)):
-        A[i,:2] = ips[i].pt
-        A[i,2] = ips[i].angle
-        A[i,3] = ips[i].size
-        A[i,4] = ips[i].octave
-        A[i,5] = ips[i].response
-        A[i,6] = ips[i].class_id
-
-    return A
-
-def localfeatures_save(ips, lfs, imageFile, dataDir, detector, descriptor):
-    """Saves local feature file in the data directory"""
-
-    # Construct a filename and path for the feature file
-    dataFile = os.path.join(dataDir,
-                            'localfeatures',
-                            detector + '+' + descriptor,
-                            imageFile)
-
-    # Convert OpenCV keypoints to a matrix
-    ips = localfeatures_kp2array(ips)
-
-    # Make sure that the output directory exists
-    dataDir = os.path.dirname(dataFile)
-    if not os.path.exists(dataDir):
-        os.makedirs(dataDir)
-
-    # Store the file using numpy
-    np.save(dataFile+'.ip.npy', ips)
-    np.save(dataFile+'.lf.npy', lfs)
-
+sys.path.append('..' + os.path.sep)
+from datasets import *
 
 def main(argv):
     """Main function for running extract features script"""
@@ -58,33 +20,16 @@ def main(argv):
     parser.add_argument('-il', '--imageList', default=None, help='Input image list file')
     parser.add_argument('-ip', '--detector', default="HARRIS", help='Local feature detector (HARRIS, SIFT, ...)')
     parser.add_argument('-lf', '--descriptor', default="SIFT", help="Local feature descriptor")
-
+    parser.add_argument('--debug', default=0, help="Debug level")
     args = parser.parse_args()
 
     # Get a list of images
-    if args.imageList is None:
-        # If user wants to extract features from a single image
-        if args.imageDir.endswith('.jpg') or args.imageDir.endswith('.png'):
-            imgList = [args.imageDir]
-        else:
-            # Get a list of images from the given directory
-            imgList = glob.glob(args.imageDir + '*/*.jpg')
-    else:
-        # Read the given imageList
-        imgList = open(args.imageList).read()
+    imageSet = ImageCollection(imgDir=args.imageDir,
+                                dataDir=args.dataDir,
+                                ipdetector=args.detector,
+                                lfdescriptor=args.descriptor)
 
-    # This is the actual imageDir in every case
-    imgDir = os.path.dirname(args.imageDir)
-
-    # Start extracting features and saving them in the
-    for imgid in range(0, len(imgList)):
-        imageFile = imgList[imgid]
-        sys.stdout.write("Processing %s (%d/%d)\r" % (imageFile, imgid, len(imgList)))
-        imageFilePath = os.path.join(imageFile[len(imgDir)+1:])
-        [ips, lfs] = lf.extractfeatures(imageFile,
-                                    detector=args.detector,
-                                    descriptor=args.descriptor)
-        localfeatures_save(ips, lfs, imageFilePath, args.dataDir, args.detector, args.descriptor)
+    imageSet.localfeatures_extract(debuglevel=args.debug)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
--- tools/generatecodebook.py
+++ tools/generatecodebook.py
@@ -10,10 +10,8 @@ import glob
 import random
 
 # Append vocpy scripts
-sys.path.append('../')
-from localfeatures import LocalFeatures as lf
-from codebook import Codebook
-
+sys.path.append('..' + os.path.sep)
+from datasets import *
 
 def localfeatures_load(imageFile, dataDir, detector, descriptor):
     """Loads local feature file from the data directory"""
@@ -65,48 +63,21 @@ def main(argv):
 
     print(args)
 
-    # Init codebook object
-    codebook = Codebook(detector=args.detector,
-                        descriptor=args.descriptor,
-                        codebooksize=args.codebooksize,
-                        codebookmethod=args.codebookmethod)
+    # Get imageSet object, which takes actual processing of features etc
+    imageSet = ImageCollection(imgDir=args.imageDir,
+                                dataDir=args.dataDir,
+                                ipdetector=args.detector,
+                                lfdescriptor=args.descriptor,
+                                codebookmethod=args.codebookmethod,
+                                codebooksize=args.codebooksize)
 
-    # Get a list of images
-    if args.imageList is None:
-        # Get a list of images from the given directory
-        imgList = glob.glob(args.imageDir + '*/*.jpg')
-    else:
-        # Read the given imageList
-        imgList = open(args.imageList).read()
+    N = len(imageSet.imageNames)
 
-    # If the number of images to be used for codebook generation is smaller
-    # than the number of images available, then choose randomly a subset
-    if int(args.nimages) < len(imgList):
-        random.shuffle(imgList)
-        imgList = random.sample(imgList, int(args.nimages))
+    if N > int(args.nimages):
+        imageSet.imageNames = random.sample(imageSet.imageNames, int(args.nimages))
+        N = len(imageSet.imageNames)
 
-    print(len(imgList))
-
-    # Read local features
-    features = []
-    for imgid in range(0, len(imgList)):
-        sys.stdout.write("Loading local features.. %d/%d\r" % (imgid, len(imgList)))
-        imageFile = imgList[imgid]
-        imageFilePath = os.path.join(imageFile[len(args.imageDir):])
-        [ips, lfs] = localfeatures_load(imageFilePath,
-                                        args.dataDir,
-                                        args.detector,
-                                        args.descriptor)
-        features.append(lfs)
-    print("\n\t* DONE!")
-    features = np.vstack(features)
-
-    print features.shape
-
-    print("Generating a codebook (ip: %s; lf: %s; method: %s; size: %d" %
-        (args.detector, args.descriptor, args.codebookmethod, args.codebooksize))
-    cbs = codebook.generate(features)
-    codebook_save(cbs, args.dataDir, args.detector, args.descriptor, args.codebookmethod, args.codebooksize)
+    imageSet.codebook_generate()
 
 if __name__ == '__main__':
     main(sys.argv[1:])
