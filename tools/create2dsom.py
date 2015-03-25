5f5e38f96ba1ff9f5363fb4d8d8114cd059e0ab6
Teemu Kinnunen <jte.kinnunen@gmail.com>
2015-03-25 14:45:04 +0200

    Added new function to visualise an image set on a 2D map using som (SOMPY)

---
+++ tools/create2dsom.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python2
+
+# -*- coding: utf-8 -*-
+
+NUMTHREADS = 4
+PICRES = 50
+
+import os
+import sys
+import argparse
+import numpy as np
+import glob
+import pylab
+import sklearn.preprocessing
+import scipy.misc as sm
+
+# Append vocpy scripts
+sys.path.append('..' + os.path.sep)
+from datasets import *
+
+# Import SOMPY
+#TODO: fix path if necessary ;)
+sys.path.append(os.path.join('..', '..', 'SOMPY'))
+from SOMPY import SOM
+
+def codebookhistogram_matrix_save(cbm, imgList, dataDir, detector, descriptor, codebookmethod, codebooksize):
+    "Saves codebook histograms matrix"
+
+    dataFile = os.path.join(dataDir, 'codehistograms_matrices',
+        detector + '+' + descriptor + codebookmethod + '+' + str(codebooksize))
+
+    if not os.path.exists(os.path.dirname(dataFile)):
+        os.makedirs(os.path.dirname(dataFile))
+
+    np.save(dataFile, cbm)
+
+    return 0
+
+
+def codebookhistogram_matrix_load(dataDir, detector, descriptor, codebookmethod, codebooksize):
+    "Loads codebook histograms matrix"
+
+    dataFile = os.path.join(dataDir, 'codehistograms_matrices',
+        detector + '+' + descriptor + codebookmethod + '+' + str(codebooksize))
+
+    if not os.path.exists(dataFile):
+        return None
+
+    cbm = np.load(dataFile)
+
+    return cbm
+
+
+def main(argv):
+    """Main function for running extract features script"""
+
+    # Parse command line parameters
+    parser = argparse.ArgumentParser(description='This script extracts visual features from given images')
+    parser.add_argument('-i', '--imageDir', help="Path to the images", required=True)
+    parser.add_argument('-dd', '--dataDir', help='Data directory where data will be stored', required=True)
+    parser.add_argument('-il', '--imageList', default=None, help='Input image list file')
+    parser.add_argument('-ip', '--detector', default="HARRIS", help='Local feature detector (HARRIS, SIFT, ...)')
+    parser.add_argument('-lf', '--descriptor', default="SIFT", help="Local feature descriptor")
+    parser.add_argument('-k', '--codebooksize', default=1000, help='Size of the codebook')
+    parser.add_argument('-cm', '--codebookmethod', default="MiniBatchKMeans", help="Codebook generation method")
+    parser.add_argument('-c', '--clusters', default=0, help='Number of image clusters')
+    parser.add_argument('-ln', '--LNormalization', default=2, help="Codebook histogram normalization (0=none, 1=L1, 2=L2)")
+    parser.add_argument('-sx', '--SOMX', default=50, help='Number of horizontal units SOM')
+    parser.add_argument('-sy', '--SOMY', default=30, help='Number of vertical units SOM')
+    parser.add_argument('--debug', default=0, help='Debugging level')
+
+    args = parser.parse_args()
+
+    # Get imageSet object, which takes actual processing of features etc
+    imageSet = ImageCollection(imgDir=args.imageDir,
+                                dataDir=args.dataDir,
+                                ipdetector=args.detector,
+                                lfdescriptor=args.descriptor,
+                                codebookmethod=args.codebookmethod,
+                                codebooksize=args.codebooksize)
+
+    # If user gave as an image list file, we must update the imageSet
+    if args.imageList is not None:
+        imageSet.read_imagelist(args.imageList)
+
+    # Number of images
+    N = len(imageSet.imageNames)
+
+    # Try to load codebook histogram matrix
+    cbm = codebookhistogram_matrix_load(args.dataDir, args.detector,
+                                        args.descriptor, args.codebookmethod,
+                                        args.codebooksize)
+
+    # Read codebook histograms
+    cbm = np.zeros((N, args.codebooksize))
+    for imgid in range(0, N):
+        sys.stdout.write("Loading codebook histograms.. %d/%d\r" %
+                            (imgid, N))
+        cbhistogram = imageSet.codebookhistograms_load(imageSet.imageNames[imgid])
+        cbm[imgid, :] = cbhistogram
+    print("\n\t * Done!")
+
+    # Save because people typically hate to wait :)
+    codebookhistogram_matrix_save(cbm, imageSet.imageNames, args.dataDir,
+                                    args.detector, args.descriptor,
+                                    args.codebookmethod, args.codebooksize)
+
+    # Normalize codebook histograms (each sample independently)
+    cbm = sklearn.preprocessing.normalize(cbm, 'l'+str(args.LNormalization),
+                                            axis=1)
+
+    ## Visualize image set using 2D SOM
+    # Train SOM
+    isom = SOM('imagemap', cbm, mapsize = (int(args.SOMX), int(args.SOMY)))
+    isom.train(trainlen=5)
+    # Map images to xy on the map
+    xy = isom.ind_to_xy(isom.bmu[0])
+    # Sort images based on their distance to the map to find the most suitable
+    # images for visualization
+    d = isom.bmu[1]
+    idx = np.argsort(d)
+    imgmap = np.zeros(isom.mapsize)-1
+    for i in idx:
+        if imgmap[xy[i,0],xy[i,1]] == -1:
+            imgmap[xy[i,0],xy[i,1]] = idx[i]
+    # Generate SOM image
+    I = np.zeros((isom.mapsize[0]*PICRES, isom.mapsize[1]*PICRES, 3), dtype=np.uint8)
+    for sy in range(0, isom.mapsize[0]):
+        offset_y = sy * PICRES
+        for sx in range(0, isom.mapsize[1]):
+            offset_x = sx * PICRES
+            #
+            imgid = int(imgmap[sy,sx])
+            subimage = pylab.imread(os.path.join(args.imageDir,
+                                                imageSet.imageNames[imgid]))
+            subimage = sm.imresize(subimage, (PICRES, PICRES))
+            I[offset_y:offset_y+PICRES,offset_x:offset_x+PICRES,:] = subimage
+    pylab.imshow(I)
+    pylab.pause(60)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
